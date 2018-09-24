82a6322f5b0e201e034d312e4281df70b842a17f
Sivar Sarkawt <sivar.sarkawt@engineer.com>
2018-09-24 21:04:27 +0300

    Add header images to the post

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -3,7 +3,7 @@
 
 @section('header')
 <!-- Page Header -->
-<header class="masthead" style="background-image: url('{{asset ('clean/img/post-bg.jpg')}}')">
+<header class="masthead" style="background-image: url('{{ asset('storage/'.$post->image) }}')">
     <div class="overlay"></div>
     <div class="container">
       <div class="row">
@@ -26,7 +26,7 @@
   <article>
     <div class="container">
       <div class="row">
-        <div class="col-lg-8 col-md-10 mx-auto">
+        <div class="col-lg-10 col-md-10 mx-auto">
           {!! $post->body !!}
         </div>
       </div>
